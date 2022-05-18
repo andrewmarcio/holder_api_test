b82350d82a03fce542b4992e78dfcfc98443ee7c
andrew.marcio <andrewmarcio21@gmail.com>
2022-05-18 01:56:30 -0300

    Add Migration

---
+++ database/migrations/2022_05_18_032540_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->date("initial_date");
+            $table->time("initial_hour");
+            $table->date("final_date");
+            $table->time("final_hour");
+            $table->text("description");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
