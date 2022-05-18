f57a67c622e4b57689406c3629952a093b7ff658
andrew.marcio <andrewmarcio21@gmail.com>
2022-05-18 13:30:05 -0300

    Fix NOT NULL columns in tasks table

---
+++ database/migrations/2022_05_18_050534_change_final_date_and_hour_column_to_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeFinalDateAndHourColumnToTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            DB::statement("ALTER TABLE tasks MODIFY final_date DATE NULL");
+            DB::statement("ALTER TABLE tasks MODIFY final_hour TIME NULL");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            DB::statement("ALTER TABLE tasks MODIFY final_date DATE NOT NULL");
+            DB::statement("ALTER TABLE tasks MODIFY final_hour TIME NOT NULL");
+        });
+    }
+}
