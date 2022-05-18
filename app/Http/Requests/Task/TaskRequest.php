8a68aa4485786e26825d2b46e9cfe2f5fc85f7f4
andrew.marcio <andrewmarcio21@gmail.com>
2022-05-18 02:48:40 -0300

    Fix request validation errors

--- app/Http/Requests/Task/TaskRequest.php
+++ app/Http/Requests/Task/TaskRequest.php
@@ -25,9 +25,9 @@ class TaskRequest extends FormRequest
     {
         return [
             "initial_date" => "required|date|date_format:Y-m-d",
-            "initial_hour" => "required|date|date_format:h:i",
+            "initial_hour" => "required|date_format:h:i",
             "final_date" => "nullable|date|date_format:Y-m-d|after_or_equal:initial_date",
-            "final_hour" => "required|date|date_format:h:i|after:initial_hour",
+            "final_hour" => "nullable|date_format:h:i|after:initial_hour",
             "description" => "required|string"
         ];
     }
