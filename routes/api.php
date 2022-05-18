4e5001150bd0b19b0f1cbce31a601c1da2a64de4
andrew.marcio <andrewmarcio21@gmail.com>
2022-05-18 13:25:31 -0300

    Add routes to application

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix("tasks")->group(function(){
+    Route::get("/", App\Http\Controllers\Task\IndexController::class)->name("task.index");
+    Route::get("/{taskId}", App\Http\Controllers\Task\ShowController::class)->name("task.show");
+    Route::post("/", App\Http\Controllers\Task\StoreController::class)->name("task.store");
+    Route::put("/{taskId}", App\Http\Controllers\Task\UpdateController::class)->name("task.update");
+    Route::delete("/{taskId}", App\Http\Controllers\Task\DeleteController::class)->name("task.delete");
 });
