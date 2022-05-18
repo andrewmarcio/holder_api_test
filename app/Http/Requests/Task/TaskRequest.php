6b5f01db87c2679863c8db5250c92b29d7ae9a1a
andrew.marcio <andrewmarcio21@gmail.com>
2022-05-18 02:02:16 -0300

    Make request validation

---
+++ app/Http/Requests/Task/TaskRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Task;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "initial_date" => "required|date|date_format:Y-m-d",
+            "initial_hour" => "required|date|date_format:h:i",
+            "final_date" => "nullable|date|date_format:Y-m-d|after_or_equal:initial_date",
+            "final_hour" => "required|date|date_format:h:i|after:initial_hour",
+            "description" => "required|string"
+        ];
+    }
+}
